568edbcaa7897dc159f839b8a9f77d8ab800e37b
wasder <wasdeg@gmail.com>
2020-03-18 21:38:50 +0300

    +testGetClassNumber

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,4 +1,11 @@
 public class MainClass {
+
+    private int classNumber = 20;
+
+    public int getClassNumber(){
+        return this.classNumber;
+    }
+
     public int getLocalNumber(){
         return 14;
     }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MainClassTest {
 
@@ -9,5 +9,12 @@ class MainClassTest {
         int expected = 14;
         int actual = new MainClass().getLocalNumber();
         assertEquals(expected, actual ,"Method getLocalNumber() doesn't return " + expected);
+    }
+
+    @Test
+    void testGetClassNumber() {
+        int expected = 45;
+        int actual = new MainClass().getClassNumber();
+        assertTrue(actual > expected, "Method getClassNumber() returns " + actual + " which is less than " + expected);
     }
 }
